bd4801d58e25efaa00d1f637e0a10e0c54b86933
Travis Parker <tparker@lunas>
2019-12-01 05:07:06 +0000

    move request/respone code to Client, both Node and clients will use it

--- meeseeks/node.py
+++ meeseeks/node.py
@@ -3,8 +3,8 @@
 import time
 import threading
 import logging
-import json
-import socket, ssl
+
+from .client import Client
 
 class Node(threading.Thread):
     '''node poller/state sync thread
@@ -15,51 +15,20 @@ class Node(threading.Thread):
         self.state=state
         threading.Thread.__init__(self,daemon=True,name='Node.'+self.remote_node,target=self.node_run)
         self.logger=logging.getLogger(self.name)
-        self.address=address
-        if not self.address: self.address=self.node
-        self.port=port
-        self.timeout=timeout
+        if not address: address=self.node
         self.refresh=refresh
-        self.__socket=None
-        self.cfg=cfg
         self.shutdown=threading.Event()
+        self.__client=Client(
+            remote_node=remote_node,
+            address=address,
+            port=port,
+            timeout=timeout,
+            **cfg)
         self.start()
 
-    def __sr(self,requests):
-        #conected and send/recieve request/response
-        if not self.__socket:
-            self.logger.debug('connecting to %s:%s'%(self.address,self.port))
-            try: 
-                self.__socket=socket.create_connection((self.address,self.port),timeout=self.timeout)
-                sslcfg=self.cfg.get('ssl')
-                if sslcfg:
-                    self.__socket = ssl.wrap_socket(self.__socket,
-                        certfile = sslcfg.get('certfile'),
-                        keyfile = sslcfg.get('keyfile'),
-                        ca_certs = sslcfg.get('ca_certs') )
-            except Exception as e:
-                if self.__socket is not False:
-                    self.logger.warning("%s:%s %s"%(self.address,self.port,e))
-                    self.__socket=False #suppress repeated warnings
-            if self.__socket: self.logger.info('connected to %s:%s'%(self.address,self.port))
-        if self.__socket:
-            try:
-                self.__socket.sendall(json.dumps(requests).encode())
-                self.__socket.sendall('\n'.encode())
-                l=''
-                while True:
-                    l+=self.__socket.recv(65535).decode()
-                    if '\n' in l: return json.loads(l)
-            except Exception as e: 
-                self.logger.warning(e,exc_info=True)
-                if self.__socket: 
-                    self.__socket.close()
-                    self.__socket=None
-
     def node_run(self):
+        ts=0
         while not self.shutdown.is_set():
-            if not self.__socket: ts=0 #reset ts to push all state on reconnect
-            
             #we sync updates for all nodes that are routed through the remote node
             sync=dict( (jid,job) for (jid,job) in self.state.get(ts=ts).items() \
                         if job['node'] != self.node and job['node'] in \
@@ -72,7 +41,8 @@ class Node(threading.Thread):
                 'sync':sync,
                 'get':{'ts':ts}
             }
-            responses=self.__sr([request])
+
+            responses=self.__client.request([request])
             updated=None
             if responses:
                 response=responses[0]
@@ -80,7 +50,8 @@ class Node(threading.Thread):
                     response.get('get',{}),
                     response.get('status',{}),
                     remote_node=self.remote_node)
-            if responses: self.logger.debug('%s sent %s, updated %s'%(ts,len(sync),len(updated)))
-            ts=time.time()-self.refresh #set the next window to go back to one refresh period ago
+                self.logger.debug('%s sent %s, updated %s'%(ts,len(sync),len(updated)))
+                ts=time.time()-self.refresh #set the next window to go back to one refresh period ago
+            else: ts=0 #if client did not respond, reset ts to sync all on reconnect
             time.sleep(self.refresh) 
-        if self.__socket:self.__socket.close()
+        self.__client.close()
